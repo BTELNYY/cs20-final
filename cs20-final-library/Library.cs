3c664cc4ad3bdce4cde15bf20abc3b883ab15485
BTELNYY <btelnyy@gmail.com>
2023-05-19 15:08:31 -0600

    server and packet stuffs

---
+++ cs20-final-library/Library.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System;
+
+namespace cs20_final_library
+{
+    public class Packet
+    {
+        public virtual uint PacketID { get; private set; }
+
+        public Packet(uint packetID)
+        {
+            PacketID = packetID;
+        }
+
+        public Packet() { }
+    }
+
+    public static class Utility
+    {
+        public static Packet GetPacketFromBytes(byte[] bytes)
+        {
+            Packet packet = new Packet(BitConverter.ToUInt32(bytes, 0));
+            uint id = packet.PacketID;
+            switch (id)
+            {
+                case 0:
+                    return new Packet(id);
+
+                default:
+                    return new Packet(uint.MaxValue);
+            }
+        }
+    }
+}
---
+++ cs20-final/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace cs20_final_server;
+
+public class Program
+{
+    //http://csharp.net-informations.com/communications/csharp-multi-threaded-server-socket.htm
+
+    public static void Main(string[] args)
+    {
+        TcpListener serverSocket = new TcpListener(IPAddress.Parse("127.0.0.1") ,8888);
+        TcpClient clientSocket = default;
+        uint counter = 0;
+
+        serverSocket.Start();
+        Console.WriteLine("Server Started");
+
+        counter = 0;
+        while (true)
+        {
+            counter += 1;
+            clientSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine("Client connected. ID: " + counter.ToString());
+            Client client = new Client();
+            client.startClient(clientSocket, counter);
+        }
+
+        clientSocket.Close();
+        serverSocket.Stop();
+        Console.WriteLine(" >> " + "exit");
+        Console.ReadLine();
+    }
+
+    public class Client
+    {
+        TcpClient clientSocket = new();
+        uint clNo = 0;
+        public void startClient(TcpClient inClientSocket, uint clientNum)
+        {
+            this.clientSocket = inClientSocket;
+            this.clNo = clientNum;
+            Thread ctThread = new Thread(handleClient);
+            ctThread.Start();
+        }
+
+        private void handleClient()
+        {
+
+        }
+    }
+}
