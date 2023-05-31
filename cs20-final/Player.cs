e4e9993a1add1cd77c288f4717cdf9f134fccb41
BTELNYY <btelnyy@gmail.com>
2023-05-31 15:12:52 -0600

    Create Player.cs

---
+++ cs20-final/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs20_final
+{
+    public class Player
+    {
+        public Client PlayerClient { get; private set; }
+
+        public string Name = "";
+        public uint AssignedClientId = 0;
+    }
+}
